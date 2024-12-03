09565289d21c0ab2c48a8722a528238713e1327c
prabhavathi2501 <prabhashok2501@gmail.com>
2024-12-03 19:14:40 +0530

    update

--- ITEM/src/App.jsx
+++ ITEM/src/App.jsx
@@ -3,103 +3,103 @@ import { useState } from 'react';
 import "./App.css"
 
 
-function intersection(a,b){
-  return a.filter((value)=>b.indexOf((value)!==-1))
+function intersection(a, b) {
+  return a.filter((value) => b.indexOf((value) !== -1))
 
 }
 
-function not(a,b){
-  return a.filter((value)=>b.indexOf(value)=== -1)
+function not(a, b) {
+  return a.filter((value) => b.indexOf(value) === -1)
 }
 
 function App() {
-  let[checked,setChecked]=useState([]);
-  let[left,setLeft]=useState([0,1,2,3]);
-  let[right,setRight]=useState([4,5,6,7]);
+  let [checked, setChecked] = useState([]);
+  let [left, setLeft] = useState([0, 1, 2, 3]);
+  let [right, setRight] = useState([4, 5, 6, 7]);
 
-  const leftChecked = intersection(checked,left);
-  const rightChecked = intersection(checked,right);
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
   // console.log("leftchc",leftChecked)
 
-  const handleToggle=(value)=>{
-    const currentIndex=checked.indexOf(value);
-    const newChecked=[...checked]
+  const handleToggle = (value) => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked]
 
-    if(currentIndex === -1){
+    if (currentIndex === -1) {
       newChecked.push(value)
-    }else{
-      newChecked.splice(currentIndex,1)
+    } else {
+      newChecked.splice(currentIndex, 1)
     }
     setChecked(newChecked);
   };
   // console.log(checked);
-  const handleAllRight=()=>{
+  const handleAllRight = () => {
     setRight(right.concat(left))
     setLeft([])
 
   }
 
-  const handleAllLeft=()=>{
+  const handleAllLeft = () => {
     setLeft(left.concat(right));
     setRight([]);
   }
 
 
-const handleCheckLeft=()=>{
-  setLeft(left.concat(rightChecked));
-  setRight(not(right,rightChecked))
-  setChecked(not(checked,rightChecked))
+  const handleCheckLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked))
+    setChecked(not(checked, rightChecked))
 
-}
+  }
 
-const handleCheckRight=()=>{
-  setRight(right.concat(leftChecked));
-  setLeft(not(left,leftChecked))
-  setChecked(not(checked,leftChecked))
-}
+  const handleCheckRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked))
+    setChecked(not(checked, leftChecked))
+  }
 
 
 
-  const customlist=(item)=>(
+  const customlist = (item) => (
     <div className='list'>
       <ul>
-       {
-        item.map((value)=>{
-          const labelId=`tranfer-list-item- ${value}
+        {
+          item.map((value) => {
+            const labelId = `tranfer-list-item- ${value}
           -label`
-          return(
-            <li key={value}>
-              <input type="checkbox"
-              onChange={()=>handleToggle(value)}
-              checked={checked.indexOf(value) !== -1}
-               id={labelId}/>
-              <label htmlFor='labelId'>Item {value+1}</label>
-            </li>
-           )
-        })
-      
-       }
-      
+            return (
+              <li key={value}>
+                <input type="checkbox"
+                  onChange={() => handleToggle(value)}
+                  checked={checked.indexOf(value) !== -1}
+                  id={labelId} />
+                <label htmlFor='labelId'>Item {value + 1}</label>
+              </li>
+            )
+          })
+
+        }
+
       </ul>
 
     </div>
   )
 
-  
-  return <>
- <div className='tranfer-list'>
-  <div className='list-container'>{customlist(left)}</div>
-  <div className='button-container'>
-   
-    <button onClick={handleCheckRight}> ADD &gt;</button>
-    <button onClick={handleCheckLeft}>Remove &lt;</button>
-    <button onClick={handleAllRight}> Add All&gt;&gt;</button>
-    <button onClick={handleAllLeft}>Remove All&lt;&lt;</button>
-   
-  </div>
-  <div className='list-container'>{customlist(right)}</div>
 
- </div>
+  return <>
+    <div className='tranfer-list'>
+      <div className='list-container'>{customlist(left)}</div>
+      <div className='button-container'>
+
+        <button onClick={handleCheckRight}> ADD &gt;</button>
+        <button onClick={handleCheckLeft}>Remove &lt;</button>
+        <button onClick={handleAllRight}> Add All&gt;&gt;</button>
+        <button onClick={handleAllLeft}>Remove All&lt;&lt;</button>
+
+      </div>
+      <div className='list-container'>{customlist(right)}</div>
+
+    </div>
   </>
 }
 
